e8333ce4d429fe37769dd0283d1753dfdc4ff548
Avatarui <162191182+Avatarui@users.noreply.github.com>
2024-03-12 16:39:33 +0700

    Update app.ts

--- app.ts
+++ app.ts
@@ -4,7 +4,7 @@ import cors from "cors";
 import { router as login } from "./api/login";
 import { router as picture } from "./api/picture";
 import { router as vote } from "./api/vote";
-
+import { router as rating } from "./api/rating";
 export const app = express();
 app.use(
     cors({
@@ -16,3 +16,4 @@ app.use(bodyParser.json());
 app.use("/login", login);
 app.use("/picture", picture);
 app.use("/vote", vote);
+app.use("/rating",rating);
